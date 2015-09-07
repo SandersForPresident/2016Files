ad8e48fbe3f8dd11f74ca74e89bdd19d97fd85d3
Jeffery Yeary <jeff@debug.ninja>
2015-09-06 20:09:31 -0400

    changing some ruby style stuff

--- parsing_script/parse.rb
+++ parsing_script/parse.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-#usage bundle exec ruby parse.rb http://transcripts.cnn.com/TRANSCRIPTS/2015.08.26.html
+# usage bundle exec ruby parse.rb http://transcripts.cnn.com/TRANSCRIPTS/2015.08.26.html
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
@@ -9,8 +9,8 @@ require 'uri'
 require '../parsing_script/lib/candidate_quotes/msnbc_parser'
 require '../parsing_script/lib/candidate_quotes/abc_parser'
 
-#CandidateQuotes.crawl_cnn(ARGV[0])
+# CandidateQuotes.crawl_cnn(ARGV[0])
 
-#puts CandidateQuotes::MSNBCParser.new('http://www.nbcnews.com/id/57810297/ns/msnbc-hardball_with_chris_matthews/').transcript.to_json
+# puts CandidateQuotes::MSNBCParser.new('http://www.nbcnews.com/id/57810297/ns/msnbc-hardball_with_chris_matthews/').transcript.to_json
 
 puts CandidateQuotes::ABCParser.new('http://abcnews.go.com/Politics/week-transcript-sen-bernie-sanders-gov-bobby-jindal/story?id=33383476&singlePage=true').transcript.to_json
